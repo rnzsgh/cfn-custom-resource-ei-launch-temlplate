4c42873f2ff43848803c066e7d5eef30aefb8b31
Ryan <ryannitz@amazon.com>
2019-05-03 20:37:52 -0400

    Add tag

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,5 @@ GOOS=linux go build main.go
 zip cfn-custom-resource-ei-launch-template.zip ./main
 
 aws s3 cp cfn-custom-resource-ei-launch-template.zip s3://$BUCKET_NAME/cfn-custom-resource-ei-launch-template.zip
+
+aws s3api put-object-tagging --bucket $BUCKET_NAME --key cfn-custom-resource-ei-launch-template.zip --tagging 'TagSet={Key=public,Value=yes}'
